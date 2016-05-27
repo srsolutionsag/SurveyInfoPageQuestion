6d849ac50d7a941960696eb03266fc4ed1c12265
Fabian Schmid <fs@studer-raimann.ch>
2016-05-27 15:19:25 +0200

    plugin version

--- plugin.php
+++ plugin.php
@@ -1,8 +1,7 @@
 <?php
 $id = 'infopage';
-$version = '1.1.0';
-$ilias_min_version = '4.4.0';
-$ilias_max_version = '5.0.999';
-$responsible = 'Fabian Schmid';
-$responsible_mail = 'fs@studer-raimann.ch';
-?>
+$version = '1.1.1';
+$ilias_min_version = '5.0.0';
+$ilias_max_version = '5.1.999';
+$responsible = 'PHZH';
+$responsible_mail = 'pascal.schmitt@phzh.ch';
